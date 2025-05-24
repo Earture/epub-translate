522a517de7c934ec54cba43113b8697203dc28bd
SpaceShaman <spaceshaman@tuta.io>
2025-05-24 14:35:16 +0200

    ♻️ (main.py): remove redundant _check_file_exists function to simplify code and rely on epub.read_epub's built-in error handling

--- epub_translate/main.py
+++ epub_translate/main.py
@@ -1,10 +1,7 @@
-import os
-
 from ebooklib import epub
 
 
 def translate(file_path: str, target_language: str) -> None:
-    _check_file_exists(file_path)
     book = epub.read_epub(file_path)
     _set_new_language(book, target_language)
     new_file_path = f"{file_path.replace('.epub', '')}_{target_language}.epub"
@@ -16,8 +13,3 @@ def _set_new_language(book: epub.EpubBook, target_language: str) -> None:
         if "language" in data:
             data["language"].clear()
     book.set_language(target_language)
-
-
-def _check_file_exists(file_path: str) -> None:
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"The file {file_path} does not exist.")
