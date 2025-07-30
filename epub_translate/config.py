f83f9185d3ab88b3f3f1fbb762c2eed151682f4e
Earture <amehinou@gmail.com>
2025-07-30 20:28:35 +0800

    Update config.py

--- epub_translate/config.py
+++ epub_translate/config.py
@@ -6,6 +6,7 @@ from pathlib import Path
 @dataclass
 class Config:
     api_key: str = ""
+    api_base: str = ""
     model: str = "gpt-4o"
 
 
@@ -15,25 +16,28 @@ def get_config() -> Config:
     if config_path.exists():
         config.read(config_path)
     else:
-        config["OpenAI"] = {"api_key": "", "model": "gpt-4o"}
+        config["OpenAI"] = {"api_key": "","api_base": "", "model": "gpt-4o"}
         with config_path.open("w") as config_file:
             config.write(config_file)
     return Config(
         api_key=config.get("OpenAI", "api_key", fallback=""),
         model=config.get("OpenAI", "model", fallback="gpt-4o"),
+        api_base=config.get("OpenAI", "api_base", fallback="http://127.0.0.1:1234/v1")
     )
 
 
-def set_config(api_key: str | None = None, model: str | None = None) -> None:
+def set_config(api_key: str | None = None,api_base: str | None = None, model: str | None = None) -> None:
     config_path = Path.home() / ".epub_translate_config"
     config = ConfigParser()
     if config_path.exists():
         config.read(config_path)
     else:
-        config["OpenAI"] = {"api_key": "", "model": "gpt-4o"}
+        config["OpenAI"] = {"api_key": "","api_base": "", "model": "gpt-4o"}
 
     if api_key is not None:
         config["OpenAI"]["api_key"] = api_key
+    if api_base is not None:
+        config["OpenAI"]["api_base"] = api_base
     if model is not None:
         config["OpenAI"]["model"] = model
 
